9b28426ce4a19d8b167918c1c835bddf19e3c03c
Arnau <abaena@elpuig.xeill.net>
2023-10-20 17:06:49 +0200

    v0.0

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,13 +3,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        ArrayList<String> listaHrefsRegiones = new ArrayList<>();
 
         System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
         FirefoxOptions options = new FirefoxOptions();
@@ -17,23 +19,43 @@ public class Main {
         WebDriver driver = new FirefoxDriver(options);
         driver.get("https://universe.leagueoflegends.com/es_ES/regions/");
 
-        // Espera implícita para dar tiempo a que la página cargue completamente
-        driver.manage().window().maximize(); // Maximiza la ventana (opcional)
-        Thread.sleep(5000); // Espera de 5 segundos (puede ser ajustada según necesidades)
+        Thread.sleep(5000);
 
         List<WebElement> elementos = driver.findElements(By.className("factionWrapper_9Uuf"));
 
-        ArrayList<String> listaHrefs = new ArrayList<>();
-
         for (WebElement elemento : elementos) {
             String href = elemento.getAttribute("href");
-            listaHrefs.add(href);
+            listaHrefsRegiones.add(href);
         }
 
-        for (String href : listaHrefs) {
-            System.out.println("Contenido de href: " + href);
+        for (String href : listaHrefsRegiones) {
+            String nombre;
+            String descripcion = "";
+            int historiasRelacionada;
+            List<String> nombreCampeones = new ArrayList<>();
+
+            driver.get(href);
+            Thread.sleep(5000);
+
+            WebElement element = driver.findElement(By.className("title_1orQ"));
+            nombre = element.getText();
+
+            element = driver.findElement(By.className("description_1-6k"));
+            List<WebElement> elements = element.findElements(By.tagName("p"));
+
+            for (WebElement elemetoss : elements) {
+                descripcion += elemetoss.getText() + "\n";
+            }
+
+            elements = driver.findElements(By.className(".QCard_hextech_root_a9df.QCard_hextech_clickable_275e.item_3MaG"));
+            historiasRelacionada = elements.size();
+
+            System.out.println("Nombre: " + nombre);
+            System.out.println("Descripción: " + descripcion);
+            System.out.println("Número de historias relacionadas: " + historiasRelacionada);
+            System.out.println("Nombres de campeones: " + Arrays.toString(nombreCampeones.toArray()));
         }
 
-        driver.quit();
+        driver.quit(); // Mueve esta línea fuera del bucle for
     }
 }
--- src/main/java/web/scraping/leagueoflegends/LoLScrap.java
+++ src/main/java/web/scraping/leagueoflegends/LoLScrap.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
+import javax.lang.model.element.Element;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,17 +36,47 @@ public class LoLScrap {
             System.out.println("Contenido de href: " + href);
         }
 
-        driver.quit();
     }
 
-    private void RobarDatosRegion(){
+    private void RobarDatos() throws InterruptedException {
+
         for (String href : listaHrefsRegiones){
+            String nombre;
+            String descripcion = null;
+            int historiasRelacionada;
+            List<String> nombreCampeones = new ArrayList<>();
+
             System.setProperty("webdriver.gecko.driver", "src/main/resources/geckodriver");
             FirefoxOptions options = new FirefoxOptions();
 
             WebDriver driver = new FirefoxDriver(options);
             driver.get(href);
+            Thread.sleep(5000);
 
+            WebElement element = driver.findElement(By.className("title_1orQ"));
+
+            nombre = element.getText();
+
+            element = driver.findElement(By.className("description_1-6k"));
+
+            List<WebElement> elementos = element.findElements(By.tagName("p"));
+
+            for (WebElement elemetoss : elementos){
+                descripcion = descripcion + elemetoss.getText() + "\n";
+            }
+
+            element = driver.findElement(By.className("moreContentBtn_2uNP"));
+            element.click();
+            elementos = driver.findElements(By.className("copy_xxN7"));
+
+            for (WebElement elemetoss : elementos){
+                nombreCampeones.add(elemetoss.findElement(By.tagName("h1")).getText());
+            }
+
+            elementos = driver.findElements(By.className("itemWrapper_22mb small_1-2z"));
+            historiasRelacionada = elementos.size();
+
+            driver.quit();
         }
     }
 }
